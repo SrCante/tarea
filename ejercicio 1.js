5eebd3e0a31bb8f0fafd24c823b7d0e20433b46e
SrCante <95399878+SrCante@users.noreply.github.com>
2021-12-05 21:55:15 -0600

    Add files via upload

---
+++ ejercicio 1.js
@@ -0,0 +1,3 @@
+let Nombre = "Lesli Yajaira Cante";
+
+console.log("Ahora estas en la matrix, ", Nombre)
---
+++ ejercicio 2.js
@@ -0,0 +1,23 @@
+let NumeroConDecimales =123.456;
+let NumeroEntero = 25;
+
+let suma = NumeroEntero + NumeroConDecimales;
+
+let ejemplo = 1;
+
+ejemplo+= 3;
+console.log(ejemplo);
+ejemplo+= 3;
+console.log(ejemplo);
+ejemplo+= 3
+console.log(ejemplo);
+
+// console.log(ejemplo);
+
+// + simbolo de la suma 
+// - simbolo de la resta
+// * simbolo para multiplicar
+// / simbolo para dividir
+// % simbolo que representa el modulo
+// ++ simbolo para aumentar en 1 el valor de la variable
+// -- simbolo para disminuir en 1 el valor de la variable
